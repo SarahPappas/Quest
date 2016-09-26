1a7114149b7f856b2466d59806068ee8afb158b5
sarahpappas <pappas.sarah@gmail.com>
2016-09-25 22:35:30 -0700

    rotation controls work

--- js/player.js
+++ js/player.js
@@ -14,7 +14,7 @@ function Player() {
 	// The noramalizes, then copies the vector of the direction the camera is looking
 	this.directionVector = this.camera.getWorldDirection().clone().normalize();
 	// The degree of rotation for each arrow press
-	this.rotation = 15;
+	this.rotation = 1;
 
 	// SETUP key controls, default false
 	this.isUpArrowActive = false;
@@ -32,18 +32,18 @@ Player.prototype = {
 	 * @param {string} arrowKey - up, down, left, or right.
 	 */
 	render: function() {
+		// console.log(this.isRightArrowActive);
 		if (this.isUpArrowActive && !this.isDownArrowActive) {
 			this._walk(this.speed);
-			console.log("up arrow is active");
 		}
 		if (this.isDownArrowActive && !this.isUpArrowActive) {
 			this._walk(-this.speed);
 		}
 		if (this.isRightArrowActive && !this.isLeftArrowActive) {
-			this._rotate(this.rotation);
+			this._rotate(-this.rotation);
 		}
 		if (this.isLeftArrowActive && !this.isRightArrowActive) {
-			this._rotate(-this.rotation);
+			this._rotate(this.rotation);
 		}
 	},
 	/**
@@ -56,8 +56,8 @@ Player.prototype = {
 	/**
 	 * @param {number} rotation in degrees - The rotation for each move. Should be positive this.rotation for spinnig right or negative this.rotation for spinning left.
 	 */
-	_rotate: function(direction) {
-		this.camera.position.x = this.camera.position.x * (direction * (Math.PI / 180));
+	_rotate: function(degrees) {
+		this.camera.rotation.y += (degrees * (Math.PI / 180));
 	},
 	_keydown: function(e) {
 		if (e.keyIdentifier == "Up") {

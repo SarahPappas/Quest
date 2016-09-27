f5a3c4230bb26193f93cca2adf698fa5b7105830
sarahpappas <pappas.sarah@gmail.com>
2016-09-27 11:41:33 -0700

    answer checking works about to do some refactering

--- js/game.js
+++ js/game.js
@@ -1,9 +1,10 @@
 function Game(world) {
 	// QUESTION would you make these constants??
 	var riddleContainerEl = $(".riddleContainer");
-	var riddlesAnsweredCorrectly = 0;
 	var submitButton = $("#answer-button");
 	this.questionEl = $(".riddleContainer .question");
+	this.correctAnswersNeeded = 3;
+	this.riddlesAnsweredCorrectly = 0;
 
 	this.riddleIndex = null;
 	this.userInput = "";
@@ -13,12 +14,14 @@ function Game(world) {
 		this._displayRiddle();
 	}.bind(this))
 
-	submitButton.click(function() {
+	submitButton.click(function(event) {
+		//FIX not working
+		event.preventDefault();
 		// save user input
 		this.userInput = $("input[name='answer']").val();
-		console.log(this.userInput);
 		//Say if you were correct and which direction to head - use question div
 		this._isRiddleCorrect();
+		this._checkNumberOfCorrectAnswers();
 		//hide the form 
 		$("form").css("display", "none");
 		//on key down hide the riddles modal?
@@ -31,21 +34,22 @@ Game.prototype = {
 		this.questionEl.text(riddles[this.riddleIndex].Question);
 		//display answer form
 		$("form").css("display", "initial");
-		console.log(this.riddleIndex);
 	},
 	_isRiddleCorrect: function() {
 		var riddleAnswer = riddles[this.riddleIndex].Answer;
-		console.log(this.riddleIndex);
-		console.log(riddleAnswer);
-		console.log(this.userInput);
-		console.log(riddleAnswer.indexOf(this.userInput));
-		// adjust for caps or lowercase
-		if (riddleAnswer.indexOf(this.userInput) == -1) {
+		if (this.userInput.toLowerCase().indexOf(riddleAnswer) == -1) {
 			this.questionEl.text("Sorry to say, but you will get no help from me");
 		} else {
 			this.questionEl.text("I'm so pleased you are correct");
+			this.riddlesAnsweredCorrectly++;
 		}
 		// remove riddle that is already shown
 		riddles.splice(this.riddleIndex, 1);
+	},
+	_checkNumberOfCorrectAnswers: function() {
+	 
+	},
+	_interactWithUser: function() {
+
 	}
 };
